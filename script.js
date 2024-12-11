4dabfae75761e946a451675027338bdc70ecc477
AgneKal <157694253+AgneKal@users.noreply.github.com>
2024-12-11 22:47:43 +0200

    upd

--- script.js
+++ script.js
@@ -132,7 +132,115 @@ let num3 = num2.valueOf();
 console.log(num2, num3);
 
 
+console.clear();
 
-// kaip paversti stringą į skaičių
 
+// MATH metodai:
+
+// 1. Apvalinimo metodai:
+// a) Math.round(x) - suapvalina iki artimiausio sveikojo skaičiaus
+
+console.log(Math.round('4.5')); //5
+console.log(Math.round(4.4)); //4
+console.log(Math.round(-4.6)); //-5
+
+// b) Math.ceil(x) - suapvalina skaičių į didesnę pusę
+
+console.log(Math.ceil(4.01)); //5
+console.log(Math.ceil(-4.6)); //-4
+
+// c) Math.floor(x) - suapvalina skaičių į mažesnę pusę
+
+console.log(Math.floor(4.01)); //4
+console.log(Math.floor(4.99)); //4
+
+// d) Math.trunc(x) - pašalina trupmeninę dalį
+
+console.log(Math.trunc(4.01)); //4
+console.log(Math.trunc(4.99)); //4
+
+// 2. Maksimumo ir minimumo paieška
+
+// a) Math.max(a, b, c) - Grąžina didžiausią iš perduotų skaičių
+
+console.log(Math.max(5, 10, 12, 7));
+console.log(Math.max()); // -Infinity
+
+// b) Math.min(a, b, c) - Grąžina mažiausią iš perduotų skaičių
+
+console.log(Math.min(5, 10, 12, 7));
+console.log(Math.min());// Infinity
+
+// 3. Atsitiktinių skaičių generavimas
+// Math.random() - grąžina atsitiktinį skaičių 0 (imtinai) iki 1 (imtinai)
+
+console.log(Math.floor(Math.random() * 10 + 1));
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+// 4. Absoliuti reikšmė
+// Math.abs(x) - grąžina absoliučią skaičiaus reikšmę
+
+console.log(Math.abs(-4.55 + 3)); // 1.5
+console.log(Math.abs(4.5)); // 4.5
+console.log(Math.abs(0)); // 0
+
+// 5. Laipsniai ir šaknys
+// a) Math.pow(skaicius, laipsnis) - pakelia skaičių laipsniu
+console.log(Math.pow(2, 3)); //8
+console.log(Math.pow(-2, 3)); //8
+console.log(Math.pow(5, 0)); //1
+
+// b) Math.sqrt(x) - grąžina kvadratinę šaknį
+console.log(Math.sqrt(9)); //3
+console.log(Math.sqrt(16)); //4
+
+// c) Math.cbrt(x) - grąžina kubinę šaknį
+console.log(Math.cbrt(27)); //3
+console.log(Math.cbrt(-8)); //4
+
+// d) Math.hypot(a, b) - Grąžina Pitagoro teoremos rezultatą
+console.log(Math.hypot(3, 4)); //5
+
+// 6. Logaritmai
+// Math.log(x); Math.log10(x); Math.log2(x);
+
+// 7. Trigonometrijos funkcijos
+// Math.sin(x); Math.cos(x); Math.tan(x);
+
+//Stringą verčiam į skaičių
+
+let stringas = '5';
+
+// 1 būdas:
+let skaicius = +stringas;
+console.log(skaicius);
+
+// 2 būdas:
+let skaicius1 = Number(stringas)
+console.log(skaicius1);
+
+// 3 būdas:
+//parseInt(); parseFloat()
+
+// 4 būdas:
+// Math apvalinimo metodai
+
+// 5 būdas:
+let num4 = JSON.parse(stringas)
+console.log(num4);
+
+//PRATIMAS
+
+
+
+console.log(5 - 2 + Number('4') - 1);
+console.log(6 + '4' * 2 + '1');
+console.log(6 + 8 + '1');
+console.log(14 + '1');
+console.log(141);
 
