aa09a7b93f980be26d7def718577694b4747cb32
AgneKal <157694253+AgneKal@users.noreply.github.com>
2024-12-12 12:39:38 +0200

    string

--- script.js
+++ script.js
@@ -234,13 +234,125 @@ console.log(skaicius1);
 let num4 = JSON.parse(stringas)
 console.log(num4);
 
-//PRATIMAS
+console.clear();
+// STRING
 
+// Kabutės
 
+const string1 = "stringas 'labas' ";
+const string2 = 'stringas';
 
-console.log(5 - 2 + Number('4') - 1);
-console.log(6 + '4' * 2 + '1');
-console.log(6 + 8 + '1');
-console.log(14 + '1');
-console.log(141);
+const string3 = `stringas ${new Date}`;
+const string4 = 'stringas' + string1;
 
+console.log(string3)
+
+// Metodai
+
+let string = 'Man labai patinka mokytis JavaScript'
+// 1. .length - apskaičiuoja ilgį (su tarpais);
+
+console.log(string.length);
+
+// 2. String'o pakeitimo metodai
+
+// a) .toUpperCase(); - pakeičia visas didžiosiomis
+console.log(string.toUpperCase());
+
+// b) .toLowerCase(); - pakeičia visas mažosiomis
+console.log(string.toLowerCase());
+
+// 3. String'o dalių paėmimas
+// a) .slice(start, end) - grąžina string'o dalį nuo start iki end
+console.log(string.slice(1)); // 'an labai patinka mokytis JavaScript'
+console.log(string.slice(-1)); // 't'
+
+console.log(string.slice(0, 3)); // [0, 3) - end parametras ne imtinai. 'Man'
+console.log(string.slice(-3, 36)); // [0, 3) - 'ipt'
+console.log(string.slice(-3, string.length)); // [0, 3) - 'ipt'
+console.log(string.slice(-36, -33)); // [0, 3) - 'Man'
+
+// b) .substring(start, end) - grąžina string'o dalį nuo start iki end
+console.log(string.substring(1)); // 'an labai patinka mokytis JavaScript'
+console.log(string.substring(0, 3)); // 'Man'
+
+// 4. Paieška string'e
+// a) .indexOf(x) - ieško ieškomo fragmento indekso
+console.log(string.indexOf('Man')); // 0
+console.log(string.indexOf('Java')); // 26, ieško pirmo sutikto!
+
+// b) .lastIndexOf(x) - ieško nuo galo
+const stringSuDviemJava = 'Man labai patinka mokytis JavaScript ir Java'
+console.log(stringSuDviemJava.lastIndexOf('Man')); // 0
+console.log(stringSuDviemJava.lastIndexOf('Java')); // 40, ieško paskutinio sutikto!
+
+// c) .include(x) - ieško argumento reikšmės ir jei randa grąžina true
+console.log(string.includes('labai')); //true
+console.log(string.includes('labas')); //false
+
+// d) .startsWith(x) - tikrina ar stringas prasideda argumento reikšme ir jei randa grąžina true
+console.log(string.startsWith('Man')); //true
+console.log(string.startsWith('labai')); //false
+
+// e) .endsWith(x) - tikrina ar stringas prasideda argumento reikšme ir jei randa grąžina true
+console.log(string.endsWith('JavaScript')); //true
+console.log(string.endsWith('cript')); //true
+console.log(string.endsWith('Man')); //false
+
+// 5. String'o pakeitimas
+
+// a) .replace(ieškoma reikšmė, kuo keičiam) - suranda PIRMĄ ieškomą reikšmę ir ją pakeičia
+console.log(string.replace('Man', 'Jums')); // Jums labai patinka mokytis JavaScript
+
+// jeigu norime, kad išliktų pakeistas
+// string = string.replace('Man', 'Jums'); - perrašau stringą
+const pakeistasStringas = ' ' + string.replace('Man', 'Jums');
+console.log(string, pakeistasStringas);
+
+// b) .replaceAll(ieškomos reikšmės, kuo keičiam) - suranda visas ieškomas reikšmes ir ją pakeičia
+console.log(stringSuDviemJava.replaceAll('Java', 'Jums')); // Man labai patinka mokytis JumsScript ir Jums
+
+// 6. String'o dalijimas ir sujungimas
+// a) .split(perKaDalinam) - padaliją stringą per nurodytą reikšmę
+console.log(string.split(' ')); // ['Man', 'labai', 'patinka', 'mokytis', 'JavaScript']
+
+console.log(string.split('')); // ['Man', 'labai', 'patinka', 'mokytis', 'JavaScript']
+
+// b) stringas.concat('kitasStringas') - sujungia du string'us į vieną
+console.log('Hello, '.concat(string)); // Hello, Man labai patinka mokytis JavaScript
+
+console.log(string.concat(pakeistasStringas))// Man labai patinka mokytis JavaScript Jums labai patinka mokytis JavaScript
+
+console.log(string.concat(' taip man ir reikia'))// Man labai patinka mokytis JavaScript taip man ir reikia
+
+// 7. String'o tarpų pradžioje ir gale panaikinimas
+// .trim()
+
+console.log(' Labas     '.trim()); //Labas
+
+// 8. Kartojimas
+// .repeat(3)
+
+console.log('miau '.repeat(5)); // 'miau miau miau miau miau '
+
+// 20 Praktinių Užduočių
+// Sukurkite stringą ir išveskite jo ilgį.
+// Konvertuokite stringą į didžiąsias raides.
+// Konvertuokite stringą į mažąsias raides.
+// Pašalinkite tarpus iš string'o pradžios ir pabaigos.
+// Patikrinkite, ar stringas prasideda žodžiu "Hello".
+// Patikrinkite, ar stringas baigiasi žodžiu "World".
+// Raskite pirmąją žodžio "Java" poziciją string'e.
+// Raskite paskutinę žodžio "Java" poziciją string'e.
+// Pakeiskite pirmąjį "Java" į "JS".
+// Pakeiskite visus "Java" į "JS".
+// Padalinkite stringą į masyvą pagal skyriklį.
+// Sujunkite du string'us į vieną.
+// Išskirkite string'o dalį nuo 3 iki 8 simbolio.
+// Naudodami slice, paimkite paskutinius 5 string'o simbolius.
+// Naudodami substring, paimkite pirmus 5 string'o simbolius.
+// Sukurkite stringą, kuris pakartojamas 4 kartus.
+// Patikrinkite, ar string'e yra žodis "fun".
+// Išveskite stringą su kabutėmis "teksto" viduje.
+// Sukurkite šablono literatūrą su kintamuoju ir išveskite rezultatą.
+// Pašalinkite tarpus tik iš string'o pradžios.
