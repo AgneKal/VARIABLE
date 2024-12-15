3dc476532f2c1d795a0ec7d8bf6ed2ebfb3c27c8
AgneKal <157694253+AgneKal@users.noreply.github.com>
2024-12-15 14:28:17 +0200

    papildomos užduotys

--- script.js
+++ script.js
@@ -371,3 +371,21 @@ console.log(pvz.padEnd(3, '0')); // '500'
 // Išveskite stringą su kabutėmis "teksto" viduje.
 // Sukurkite šablono literatūrą su kintamuoju ir išveskite rezultatą.
 // Pašalinkite tarpus tik iš string'o pradžios.
+
+
+//Papildomos užduotys (sprendimams naudokite string metodus ir for ciklą (ne visuose uždaviniuose)):
+// 1. Iš string "mokymo centras" suformatuokite taip, kad pirmos raidės būtų didžiosios, o likusios mažosios.
+// 2. Sutrumpinkite tekstą "JavaScript programavimas" iki 10 simbolių ir pridėkite '...'.
+// 3. Atspausdinkite tekstą "JavaScript" atvirkščiai.
+// 4. Tekste "obuolys, bananas, kivis" pakeiskite visus kablelius į kabliataškius.
+// 5. Suskaičiuokite, kiek yra žodžių string'e "Šiandien gera diena.".
+// 6. Patikrinkite, ar tekstas "test@gmail.com" turi @ ir baigiasi .com.
+// 7. Pašalinkite pirmą simbolį string'e " JavaScript", jei jis yra tarpas.
+// 8. Palyginkite string'us "JavaScript" ir "javascript" ignoruodami jų raidžių dydį.
+// 9. Pašalinkite visus skaičius iš teksto "a1b2c3".
+// 10. Suskaičiuokite, kiek tekste "a1b2c3" yra skaičių.
+// 11. Iš teksto "HTML CSS JS" paimkite pirmąją kiekvieno žodžio raidę ir sudėkite jas į vieną string'ą.
+// 12. Raskite pirmą žodį string'e "Hello World!".
+// 13. Pašalinkite paskutinius 3 simbolius string'e "JavaScript" ir atspausdinkite rezultatą.
+// 14. Prie string'o "Likusios dienos: " pridėkite skaičių 5 iš daysLeft kintamojo.
+// 15. Tekste "mokytojas" pakeiskite pabaigą į "ėja", kad gautumėte "mokytoja".
